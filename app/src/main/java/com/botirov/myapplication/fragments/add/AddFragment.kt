d2a22fa56d49720be2d77ac6f590dc27852fb0dd
IbrohimB <botirovibrohim@gmail.com>
2023-09-15 09:15:41 -0400

    created menu for add fragment

--- app/src/main/java/com/botirov/myapplication/fragments/add/AddFragment.kt
+++ app/src/main/java/com/botirov/myapplication/fragments/add/AddFragment.kt
@@ -3,6 +3,8 @@ package com.botirov.myapplication.fragments.add
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import com.botirov.myapplication.R
@@ -13,8 +15,13 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu,menu)
+    }
 }
--- app/src/main/java/com/botirov/myapplication/fragments/list/ListFragment.kt
+++ app/src/main/java/com/botirov/myapplication/fragments/list/ListFragment.kt
@@ -1,10 +1,12 @@
 package com.botirov.myapplication.fragments.list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.botirov.myapplication.R
 import com.botirov.myapplication.databinding.FragmentListBinding
@@ -25,11 +27,17 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
-        binding.listLayout.setOnClickListener{
+        binding.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu,menu)
     }
 
     override fun onDestroyView() {
