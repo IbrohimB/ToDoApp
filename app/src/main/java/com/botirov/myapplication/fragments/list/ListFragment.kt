78591d83c2875de60ce80387fe73779f394802d1
IbrohimB <botirovibrohim@gmail.com>
2023-10-16 10:10:36 -0400

    Delete All Added

--- app/src/main/java/com/botirov/myapplication/fragments/list/ListFragment.kt
+++ app/src/main/java/com/botirov/myapplication/fragments/list/ListFragment.kt
@@ -1,11 +1,14 @@
 package com.botirov.myapplication.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -51,6 +54,30 @@ class ListFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.list_fragment_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_delete_all){
+            confirmRemoval()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // Show AlertDialog to Confirm Removal of All Items from Database Table
+    private fun confirmRemoval() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes"){_,_ ->
+            mToDoViewModel.deleteAll()
+            Toast.makeText(
+                requireContext(),
+                "Successfully Removed Everything!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        builder.setNegativeButton("No"){_,_ ->}
+        builder.setTitle("Delete everything?")
+        builder.setMessage("Are you sure you want to remove everything?")
+        builder.create().show()
     }
 
     override fun onDestroyView() {
