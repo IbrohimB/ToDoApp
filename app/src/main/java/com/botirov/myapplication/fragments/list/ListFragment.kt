4389593dc75a179cb79412aa9a4b3dd1394d4a81
IbrohimB <botirovibrohim@gmail.com>
2023-10-17 10:02:59 -0400

    show no_data views

--- app/src/main/java/com/botirov/myapplication/fragments/list/ListFragment.kt
+++ app/src/main/java/com/botirov/myapplication/fragments/list/ListFragment.kt
@@ -17,6 +17,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.botirov.myapplication.R
 import com.botirov.myapplication.data.viewmodel.ToDoViewModel
 import com.botirov.myapplication.databinding.FragmentListBinding
+import com.botirov.myapplication.fragments.SharedViewModel
 
 class ListFragment : Fragment() {
 
@@ -24,6 +25,7 @@ class ListFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
 
     private val adapter: ListAdapter by lazy { ListAdapter() }
 
@@ -39,7 +41,12 @@ class ListFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(requireActivity())
 
         mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer {data ->
+            mSharedViewModel.checkIfDatabaseEmpty(data)
             adapter.setData(data)
+        })
+
+        mSharedViewModel.emptyDatabase.observe(viewLifecycleOwner, Observer {
+            showEmptyDatabaseViews(it)
         })
 
         binding.floatingActionButton.setOnClickListener {
@@ -50,6 +57,16 @@ class ListFragment : Fragment() {
         setHasOptionsMenu(true)
 
         return view
+    }
+
+    private fun showEmptyDatabaseViews(emptyDatabase: Boolean) {
+        if(emptyDatabase){
+            binding.noDataImageView.visibility = View.VISIBLE
+            binding.noDataTextView.visibility = View.VISIBLE
+        }else{
+            binding.noDataImageView.visibility = View.INVISIBLE
+            binding.noDataTextView.visibility = View.INVISIBLE
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
